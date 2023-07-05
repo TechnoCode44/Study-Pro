c7d1be7dfebfa817c2ac88fc46d1cc5d1d80daf2
TechnoCode44 <ElmoSmiley@outlook.com>
2023-07-05 22:13:07 +0100

    Added database get request

--- study_pro.py
+++ study_pro.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from json_loader import *
 
@@ -7,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 def index():
     return "Hello, World!"
+
+@app.get("/database/")
+def database(file):
+    file_content = load_json(f"Database/{file}")
+    
+    if file_content == None:
+        raise HTTPException(404, "File not found")
+    return file_content
