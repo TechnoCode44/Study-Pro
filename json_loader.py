a38f69e9ce05f395e458c871ae1a5c9f5dcae689
TechnoCode44 <ElmoSmiley@outlook.com>
2023-07-05 21:52:13 +0100

    Addesd save_json function

--- json_loader.py
+++ json_loader.py
@@ -1,6 +1,10 @@
-from json import load
+from json import load, dump
 
 def load_json(filename):
     file = open(filename)
     json = load(file)
     return json
+
+def save_json(dictionary, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        dump(dictionary, file, ensure_ascii=False, indent=4)
