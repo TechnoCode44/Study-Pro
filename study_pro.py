8636f12448734bda1b7c0496c8c0281a7b0e0bf4
TechnoCode44 <ElmoSmiley@outlook.com>
2023-07-11 21:16:39 +0100

    Added audio get request

--- study_pro.py
+++ study_pro.py
@@ -3,6 +3,8 @@ from fastapi.staticfiles import StaticFiles
 
 from json_loader import *
 
+from os.path import isfile
+
 app = FastAPI()
 app.mount("/audio", StaticFiles(directory="Audio"), name="Audio")
 
@@ -17,3 +19,14 @@ def database(file):
     if file_content == None:
         raise HTTPException(404, "File not found")
     return file_content
+
+@app.get("/audio")
+def audio(course: str, question_number: int):
+    path = f"Audio/{course}/Q{question_number}.mp3"
+    audio_exists = isfile(path)
+
+    if audio_exists:
+        request_path = f"/audio/{course}/Q{question_number}.mp3" # Single letter change then "path" variable
+        return request_path
+    else:
+        raise HTTPException(404, "File not found")
